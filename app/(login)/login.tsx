1afef6c979cafe3eebcb1fc90e466625100bdf74
AlexAdamov <alx.adamov@gmail.com>
2024-10-07 18:22:07 +0200

    color github signin

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -157,16 +157,14 @@ export function LoginComponent({
               <div className="w-full border-t border-gray-300" />
             </div>
             <div className="relative flex justify-center text-sm">
-              <span className="px-2 bg-gray-50 text-gray-500">
-                Or continue with
-              </span>
+              <span className="px-2 bg-gray-50 text-gray-500">Or</span>
             </div>
           </div>
 
           <div className="mt-6">
             <Button
               variant="outline"
-              className="w-full"
+              className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-full shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
               onClick={handleGithubSignIn}
             >
               <SiGithub className="mr-2 h-4 w-4" />
